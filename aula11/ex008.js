c2f3c5d54b8e4f81fccca68af682e247ab98d34a
BrunnoBarker <brunnobarker@gmail.com>
2024-01-04 10:55:44 -0300

    ex 008, 009, 010 e 011 criados

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 125.6
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 70) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')

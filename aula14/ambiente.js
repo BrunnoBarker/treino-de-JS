276c24e3221e067b308c0d894cc86814f2bf0efe
BrunnoBarker <brunnobarker@gmail.com>
2024-01-11 11:49:35 -0300

    começou... Repetições.

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1 
+do { 
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 60)
+
+
+
+
+/*console.log('olá mundo!')
+console.log('olá mundo!')
+console.log('olá mundo!')
+console.log('olá mundo!')
+console.log('olá mundo!')
+console.log('olá mundo!')
+*/
+

5d1039b73f0ec0218831fe320f7dfafc113edbe8
BrunnoBarker <brunnobarker@gmail.com>
2024-01-17 11:50:08 -0300

    terminado

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -7,11 +7,25 @@ function contar() {
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }  
+        } else {
+            //contagem decrescente
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}` 
     }
 }
